003b1f0345adf7cb6629dda376d2d800311d27eb
esaghapour <76821278+esaghapour@users.noreply.github.com>
2022-12-14 17:53:23 -0600

    Update multi-select.py

--- multi-select.py
+++ multi-select.py
@@ -11,8 +11,6 @@ st.write("You entered:", text)
 # set the API key
 openai.api_key = "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"
 
-# specify the model to use (in this case, the "text-davinci-002" model)
-model = "code-davinci-002"
 
 # specify the prompt to use
 
