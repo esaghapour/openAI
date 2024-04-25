0b30779f19ba4c87d28f806517b77e5b57fe37e8
esaghapour <76821278+esaghapour@users.noreply.github.com>
2024-04-25 11:21:51 -0500

    Update multi-select.py

--- multi-select.py
+++ multi-select.py
@@ -1,24 +1,18 @@
 import streamlit as st
 import openai
 
-st.header('How to use ChatGPT and Streamlit')
-# Set the API key for the openai library
-api_key = st.sidebar.text_input("api_key:")
-openai.api_key = api_key
+from openai import OpenAI
+client = OpenAI()
 
-# Get the text input from the user
-text = st.text_input("Enter some text:")
-
-# Use the chatGPT model to generate a response to the entered text
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=text,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
 )
 
+print(completion.choices[0].message)
+
 # Display the response in the app
-st.code(response["choices"][0]["text"], language='python')
+#st.code(response["choices"][0]["text"], language='python')
